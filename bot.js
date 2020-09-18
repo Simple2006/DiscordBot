0d62064f2bba0a29d94ef6b343c934405993b845
Simple2006 <69445064+Simple2006@users.noreply.github.com>
2020-09-18 15:33:56 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,7 +36,7 @@ bot.on("message", async message => {
         }
     }
 
-    let prefix = config.prefix;
+    let prefix = process.env.BOT_PREFIX;
     let messageArray = message.content.split(" ")
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
